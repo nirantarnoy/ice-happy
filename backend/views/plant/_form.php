9088ec66dabd1fccffb3cd9e26787046a31ed96b
tarlektarnoy <niran_tarlek@hotmail.com>
2018-11-14 09:53:01 +0700

    update

--- backend/views/plant/_form.php
+++ backend/views/plant/_form.php
@@ -9,47 +9,51 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="plant-form">
+   <div class="card">
+       <div class="card-body">
+           <?php $form = ActiveForm::begin(); ?>
 
-    <?php $form = ActiveForm::begin(); ?>
+           <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'short_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'short_name')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'eng_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'eng_name')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'tax_id')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'tax_id')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'mobile')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'mobile')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'website')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'website')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'facebook')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'facebook')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'line')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'line')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'logo')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'logo')->textInput(['maxlength' => true]) ?>
+           <?= $form->field($model, 'status')->textInput() ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+           <?= $form->field($model, 'created_at')->textInput() ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+           <?= $form->field($model, 'updated_at')->textInput() ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+           <?= $form->field($model, 'created_by')->textInput() ?>
 
-    <?= $form->field($model, 'created_by')->textInput() ?>
+           <?= $form->field($model, 'updated_by')->textInput() ?>
 
-    <?= $form->field($model, 'updated_by')->textInput() ?>
+           <div class="form-group">
+               <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+           </div>
 
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-    </div>
+           <?php ActiveForm::end(); ?>
+       </div>
+   </div>
 
-    <?php ActiveForm::end(); ?>
 
 </div>
